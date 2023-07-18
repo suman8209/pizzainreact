3c735ff6584af1f2daf3342afe13b9afb3ff7af7
suman8209 <chohansuman964@gmail.com>
2023-07-18 12:01:44 +0530

    To Do List

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Counter from "./Counter/Counter"
+import ToDo from "./TodoList/ToDo"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
-    <Counter/>
-  </>
+    <>
+        <ToDo/>   
+    </>
 );
