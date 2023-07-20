a01b4c2c10cc3408a04ba2d6fa80bfe504058a60
suman8209 <chohansuman964@gmail.com>
2023-07-20 11:08:31 +0530

    pizza

--- src/pizzadata/App.jsx
+++ src/pizzadata/App.jsx
@@ -4,32 +4,55 @@ import "./pizza.css"
 
 
 function App() {
+
+  const startinHour = 12
+  const lastHour = 22
+
+  const day = new Date()
+  const hour = day.getHours()
+
   return (
     <>
-        <div className='wrapper'>
-            {
-                pizzaData.map((pizza, index) => {
-                    return(
-                        <div className='box'>
-                            <img src={pizza.photoName} alt={pizza.name}/>
-                            <div className='box1'>
+      <header>
+        <h1>FAST REACT PIZZA CO.</h1>
+        <h3>OUR MENU</h3>
+      </header>
 
-                            
-                            <h1>{pizza.name}</h1>
-                            <p>{pizza.ingredients}</p>
-                            <p>{(pizza.soldOut === false) ? ("Price $. "+pizza.price):"soldOut"}</p>
-                            {/* <p>{pizza.soldOut}</p> */}
-                            </div>
+      <div className='wrapper'>
 
-                        </div>
+        {
+          pizzaData.map((pizza, index) => {
+            return (
 
-                        
-                    )
-                })
-            }
-        </div>
+              <div key={index} className={(pizza.soldOut !== false) ? ("gray box") : "box"}>
+                <div className='img'>
+                  <img src={pizza.photoName} alt={pizza.name} />
+                </div>
+                <div className='box1'>
+                  <h1>{pizza.name}</h1>
+                  <p>{pizza.ingredients}</p>
+                  <p>{(pizza.soldOut === false) ? ("Price $. " + pizza.price) : "soldOut"}</p>
+                </div>
+
+              </div>
+ 
+
+            )
+          })
+        }
+      </div>
+
+      <footer>
+        <p>We'er welcome you between {startinHour}:00 to {lastHour
+         }:00</p>
+
+        {(startinHour <= hour && lastHour >= hour) ? <button>Order Now</button> : null}
+
+        <div className='bottom'></div>
+
+      </footer>
     </>
-    
+
   )
 }
 
